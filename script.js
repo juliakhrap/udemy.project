d41350a72157d9ff388d757c22b827f287be3b68
Julia <juliyakhrap@gmail.com>
2022-02-09 18:37:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*const obj = {
+  name: "Tony",
+  age: 28,
+  isMarried: true,
+};
+
+console.log(obj.name);
+
+let array = ["plum.png", "orange.png", "apple.jpg"];
+console.log(array[0]);*/
+
+/*const category = "toys";
+console.log(`http://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);*/
+
+console.log("arr" + "- object");
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+const isChecked = false,
+  isClose = false;
+console.log(isChecked || !isClose);
